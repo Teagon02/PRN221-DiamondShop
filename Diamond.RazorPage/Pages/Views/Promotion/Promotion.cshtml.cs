bab0089ef0fd1a0db7041414b47dfc7c4c6d2e47
DuyPham12345 <saophadao1472@gmail.com>
2024-07-24 15:12:48 +0700

    update promotion for homepage

---
+++ Diamond.RazorPage/Pages/Views/Promotion/Promotion.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diamond.RazorPage.Pages.Views.Promotion
+{
+    public class PromotionModel : PageModel
+    {
+      
+        
+        public void OnGet()
+        {
+        
+
+        }
+    }
+}
