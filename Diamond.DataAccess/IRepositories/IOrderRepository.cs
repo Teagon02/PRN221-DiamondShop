69ca1a61aebd1add2befcca988c525499c194889
Trung <wowholyshit07@gmail.com>
2024-07-25 02:36:54 +0700

    Fix OrderRepo

---
+++ Diamond.DataAccess/IRepositories/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Diamond.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond.DataAccess.IRepositories
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetOrderByIdAsync(int orderId);
+        Task<IEnumerable<Order>> GetOrdersByUserIdAsync(int userId);
+        Task AddOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int orderId);
+    }
+}
---
+++ Diamond.DataAccess/Repositories/OrderRepository.cs
@@ -0,0 +1,61 @@
+﻿using Diamond.DataAccess.Data;
+using Diamond.DataAccess.IRepositories;
+using Diamond.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamond.DataAccess.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DiamondDbContext _context;
+
+        public OrderRepository(DiamondDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int orderId)
+        {
+            return await _context.Orders
+                                 .Include(o => o.OrderItems)
+                                 .Include(o => o.Payments)
+                                 .FirstOrDefaultAsync(o => o.OrderId == orderId);
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserIdAsync(int userId)
+        {
+            return await _context.Orders
+                                 .Include(o => o.OrderItems)
+                                 .Include(o => o.Payments)
+                                 .Where(o => o.UserId == userId)
+                                 .ToListAsync();
+        }
+
+        public async Task AddOrderAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteOrderAsync(int orderId)
+        {
+            var order = await _context.Orders.FindAsync(orderId);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
